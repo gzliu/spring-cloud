0a7b38d7b0c8ad7642637284106e67664d4b56fa
gzliu <823612018@qq.com>
2017-04-30 20:16:18 +0800

    commit config server and spring boot version to 1.3.5RELEASE

---
+++ cloud-service-config/src/main/java/com/gzliu/CloudServiceConfigApplication.java
@@ -0,0 +1,18 @@
+package com.gzliu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableConfigServer
+@EnableEurekaClient
+//@RestController
+public class CloudServiceConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudServiceConfigApplication.class, args);
+	}
+}
